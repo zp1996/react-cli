3be953291317cffcd5da72ba191add9bd7de39b3
周鹏 <1179258516@qq.com>
2017-04-25 22:47:14 +0800

    production saga demo

--- src/stores/prod.js
+++ src/stores/prod.js
@@ -1,7 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import reducer from 'Reducers/index';
+import rootSaga from 'Sagas/index';
+
+const enhancer = compose(
+    applyMiddleware([
+        createSagaMiddleware()
+    ])
+);
 
 const initialState = window.__INITIAL_STATE__,
-    store = createStore(reducer, initialState);
+    store = createStore(reducer, initialState, enhancer);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
